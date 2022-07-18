bcbfb9e29f26a96350bf339e55a64fd31b388589
Alison Silva <alisonbsilva@hotmail.com>
2022-07-18 20:44:38 -0300

    att

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -20,12 +20,18 @@ export default NextAuth({
     async signIn({ user, account, profile }) {    
       const { email } = user
 
-      await fauna.query(
-        q.Create(
-          q.Collection('users'),
-          { data: { email } }
-        )
-      )        
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+              {
+                data: { email: email }
+              }
+            )
+        )        
+          } catch (e) {
+            console.log(e)
+          }
       return true
     },  
   }
--- src/services/fauna.ts
+++ src/services/fauna.ts
@@ -1,5 +1,6 @@
 import { Client } from 'faunadb'
 
 export const fauna = new Client({
-  secret:'FAUNADB_KEY'
+  secret:'FAUNADB_KEY',
+  domain:"db.us.fauna.com"
 })
