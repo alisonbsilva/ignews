cd614626100772cbdb1b5ed72b87402bc686f40c
Alison Silva <alisonbsilva@hotmail.com>
2022-07-26 21:27:40 -0300

    Mudou o erro
    Acho que agora eu estou no caminho certo '-'

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,16 +1,15 @@
 import GithubProvider from "next-auth/providers/github"
 import { fauna } from '../../../services/fauna'
 import NextAuth from 'next-auth'
-import { Collection, query as q, Ref } from "faunadb"
-import { ifError } from "assert"
+import { query as q } from "faunadb"
 
 
 
 export default NextAuth({
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      clientId: 'GITHUB_CLIENT_ID',
+      clientSecret: 'GITHUB_CLIENT_SECRET',
       authorization: {
         params: {
           scope: "read:user",
@@ -19,7 +18,7 @@ export default NextAuth({
     }),
   ],
   jwt: {
-    secret: process.env.SIGNING_KEY
+    secret: 'SIGNING_KEY'
   },
 
   callbacks: {
